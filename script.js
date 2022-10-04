567167941aa9a4fdc6730d55e675481ae9895405
Iman Anooshehpour <imananoosheh@gmail.com>
2022-10-03 19:14:06 -0700

    Added hover event to pause interval for readablity

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 let isDataLoaded = false;
 let lastTweetLoaded = null;
+let numberOfTweets = 0;
 const tweetContainer = document.querySelector("#tweet-container");
-const mainBody = document.querySelector('#main-body')
+const mainBody = document.querySelector("#main-body");
 async function populateData() {
   const requestURL = "/data/tweets-all.json";
   const request = new Request(requestURL);
 
   const response = await fetch(request);
   const jsonData = await response.json();
+  numberOfTweets = jsonData.length;
 
   populateTweetData(jsonData);
 }
@@ -15,6 +17,7 @@ async function populateData() {
 const tweetList = [];
 function populateTweetData(objs) {
   // mainBody.appendChild(article)
+  let counter = 0;
   for (let obj of objs) {
     const article = document.createElement("article");
     article.className += "single-tweet";
@@ -22,8 +25,9 @@ function populateTweetData(objs) {
     text.className = "tweet-text";
     text.textContent = obj.Text;
     article.appendChild(text);
-    if (obj.Photos !== null) {
-      const imgContainer = document.createElement("div");
+    let imgContainer = null;
+    if (obj.Photos) {
+      imgContainer = document.createElement("div");
       imgContainer.className = "tweet-image";
       for (let imgUrl of obj.Photos) {
         const img = document.createElement("img");
@@ -32,18 +36,19 @@ function populateTweetData(objs) {
       }
       article.appendChild(imgContainer);
     }
-    if (obj.Videos !== null) {
-        if(imgContainer){
-            imgContainer.setAttribute('display', 'none')
-        }
+    if (obj.Videos) {
+      // console.log("a video exist");
+      if (imgContainer) {
+        imgContainer.style.display = "none";
+      }
       const videoContainer = document.createElement("div");
-      videoContainer.className = "tweet-image";
-
+      videoContainer.className = "tweet-video";
       const video = document.createElement("video");
       video.src = obj.Videos[0].URL;
       video.setAttribute("height", "500");
+      video.setAttribute("controls", "true");
+      video.setAttribute("muted", "");
       videoContainer.appendChild(video);
-
       article.appendChild(videoContainer);
     }
     const timestamp = document.createElement("div");
@@ -51,7 +56,17 @@ function populateTweetData(objs) {
     const unformattedTime = new Date(obj.Timestamp * 1000);
     timestamp.textContent = unformattedTime.toGMTString();
     article.appendChild(timestamp);
-    tweetList.push(article);
+    if (tweetList.length > 30) {
+      setTimeout(() => {
+        tweetList.push(article);
+      }, 100);
+    }
+    else{
+      tweetList.push(article);
+    }
+
+    counter += 1;
+    // console.log(`tweet data loaded ${Number(counter / numberOfTweets*100).toFixed(2)}%`);
   }
 }
 
@@ -59,10 +74,10 @@ populateData();
 
 function loadNTweet(startNumber = 0, endNumber = 20) {
   for (let count = startNumber; count <= endNumber; count++) {
-    if(tweetList[count]!==null){
-        tweetContainer.appendChild(tweetList[count]);
-        lastTweetLoaded = endNumber
-        console.log(lastTweetLoaded, tweetList.length)
+    if (tweetList[count] !== (null || undefined)) {
+      tweetContainer.appendChild(tweetList[count]);
+      lastTweetLoaded = endNumber;
+      // console.log(lastTweetLoaded, tweetList.length);
     }
   }
 }
@@ -71,7 +86,7 @@ const dataLoadingCheck = setInterval(() => {
   if (tweetList.length > 0) {
     isDataLoaded = true;
     setTimeout(loadNTweet(0, 0), 1000);
-    lastTweetLoaded = 0
+    lastTweetLoaded = 0;
     clearInterval(dataLoadingCheck);
   }
 }, 100);
@@ -80,35 +95,35 @@ if (isDataLoaded) {
 }
 
 function nextTweet() {
-    const newTweet = lastTweetLoaded+1
+  const newTweet = lastTweetLoaded + 1;
   let child = tweetContainer.lastElementChild;
   while (child) {
     tweetContainer.removeChild(child);
     child = tweetContainer.lastElementChild;
   }
-  loadNTweet(newTweet, newTweet)
+  loadNTweet(newTweet, newTweet);
 }
 
-const tweetChangeIntervalTime = 4000
+const tweetChangeIntervalTime = 4000;
 
-const changeTweetInterval = setInterval(()=>{
-    nextTweet()
-}, tweetChangeIntervalTime)
+let changeTweetInterval = setInterval(() => {
+  nextTweet();
+}, tweetChangeIntervalTime);
 
-function stopChangeTweetInterval(){
-    clearInterval(changeTweetInterval)
+function stopChangeTweetInterval() {
+  clearInterval(changeTweetInterval);
 }
 
-// let isMouseOverTweet = false
-// mainBody.addEventListener('mouseover',()=>{
-//     isMouseOverTweet = true
-//     stopChangeTweetInterval();
-// })
+// Stop the current timer when mouseover
+mainBody.addEventListener("mouseover", function () {
+  clearInterval(changeTweetInterval);
+  // console.log("stopped changeTweetInterval");
+});
 
-// setInterval(()=>{
-//     if(!isMouseOverTweet){
-//         changeTweetInterval = setInterval(()=>{
-//             nextTweet()
-//         }, tweetChangeIntervalTime)
-//     }
-// }, 100)
+// Start a new timer when mouse out
+mainBody.addEventListener("mouseout", function () {
+  changeTweetInterval = setInterval(() => {
+    nextTweet();
+  }, tweetChangeIntervalTime);
+  // console.log("reinstated changeTweetInterval");
+});
